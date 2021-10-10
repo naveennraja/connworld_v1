49a50a98368446602471e30e729fd1e7bfe811e5
Naveen Nata Raja <naveen@checkventory.com>
2021-10-10 14:25:35 +0100

    New Changes

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,33 +1,30 @@
-import React from "react";
+import React from 'react';
 // import Timeline from '../components/charts/Timeline';
-import ParagraphComponent from "../components/ParagraphComponent";
-import ImageComponent from "../components/ImageComponent";
-import ServicesImage from "../images/services.png";
-//simport CardboardComponent from "../components/UIComponent/CardboardComponent/CardboardComponent";
-import AccordionCardComponent from "../components/UIComponent/AccordionCardComponent/AccordionCardComponent";
+import ParagraphComponent from '../components/ParagraphComponent';import ImageComponent from "../components/ImageComponent";
+import CardboardComponent from '../components/UIComponent/CardboardComponent/CardboardComponent';
+// import AccordionCardComponent from '../components/UIComponent/AccordionCardComponent/AccordionCardComponent';
 
 const Services = (props) => {
   const {
     contentText,
     title,
     cards,
-    // cardsIcon,
-    contentTextDesign,
-    contentTextServices,
-    qualityService
+    cardsIcon,
+    contentTextDesign, 
+    contentTextServices
+   //qualityService
   } = props.contents;
   if (cards !== undefined) {
     return (
       <>
         <h3>{title}</h3>
-        <div className="row">
-          <div className="col">
+        <div className='row'>
+          <div className='col'>
             <ParagraphComponent text={contentText}></ParagraphComponent>
           </div>
         </div>
 
-        <div className="row solution-container">
-          <ImageComponent src={ServicesImage}></ImageComponent>
+        <div className='row solution-container'>
           {/* {Object.keys(cards || {}).map((ele, idx) => (
             <CardboardComponent
               key={ele}
@@ -36,23 +33,23 @@ const Services = (props) => {
               icon={cardsIcon[idx]}></CardboardComponent>
           ))} */}
         </div>
-        <div className="row">
-          <div id="accordion" className="w-100">
-            <div className="col">
+        <div className='row'>
+          <div id='accordion' className='w-100'>
+            <div className='col'>
               <ParagraphComponent
                 text={contentTextServices}></ParagraphComponent>
             </div>
-            {Object.keys(qualityService || {}).map((ele, idx) => (
+            {/* {Object.keys(qualityService || {}).map((ele, idx) => (
               <AccordionCardComponent
                 key={`ele_${idx}`}
                 textHeading={ele}
                 textContent={qualityService[ele]}
                 idx={idx}></AccordionCardComponent>
-            ))}
+            ))} */}
           </div>
         </div>
-        <div className="row">
-          <div className="col">
+        <div className='row'>
+          <div className='col'>
             <ParagraphComponent text={contentTextDesign}></ParagraphComponent>
           </div>
         </div>
